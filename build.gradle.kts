6501f533b6518158d23073b8497543a7ddcead2d
Julian Siebert <mail@julian-siebert.de>
2025-06-23 09:18:56 +0000

    fix: Github Actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import net.thebugmc.gradle.sonatypepublisher.PublishingType
+
 plugins {
     id("studio.o7.remora") version "0.0.9"
+    id("net.thebugmc.gradle.sonatype-central-portal-publisher") version "1.2.4"
 }
 
 remora {
@@ -7,8 +10,6 @@ remora {
     artifactId = "agones-sdk"
 
     description = "Agones Java gRPC SDK implementation"
-
-    mavenCentral.isEnabled = true
 
     framework {
         lombok.isEnabled = true
@@ -21,3 +22,15 @@ dependencies {
 }
 
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+
+centralPortal {
+    username = System.getenv("SONATYPE_USERNAME")
+    password = System.getenv("SONATYPE_PASSWORD")
+    name = remora.artifactId
+    publishingType = PublishingType.USER_MANAGED
+}
+
+signing {
+    useInMemoryPgpKeys(System.getenv("GPG_KEY"), System.getenv("GPG_PASSPHRASE"))
+    sign(publishing.publications)
+}
